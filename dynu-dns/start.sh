b8f9df3eca63a0df19773e8d070880290b49c505
Ahsen Baig <146462+AhsenBaig@users.noreply.github.com>
2021-04-19 18:23:53 -0700

    Updated file path.

--- dynu-dns/start.sh
+++ dynu-dns/start.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 # Copy the configuration from source to server files if it doesn't already exist.
-if [[ ! -e "/serverfiles/dynuiuc.conf" ]]; then
+if [[ ! -e "/serverfiles/ddclient.conf" ]]; then
   printf "%s\n" "Copying config"
-
-  # cp /etc/dynuiuc/dynuiuc.conf /serverfiles/dynuiuc.conf
-  cp /etc/ddclient.conf /serverfiles/ddclient.conf
+    
+  cp /etc/dynuiuc.conf /serverfiles/ddclient.conf
 fi
 
 exec /usr/sbin/ddclient -daemon 300 -syslog -file /serverfiles/ddclient.conf
